0575b20750fa6b198d070c621738c9f94af727e0
Swostik Gautam <52479279+gautamswostik@users.noreply.github.com>
2023-06-11 07:01:49 +0545

    Update weeb.router.js

--- routers/weeb.router.js
+++ routers/weeb.router.js
@@ -18,5 +18,5 @@ weeb.route("/").get(getAll).post(postData);
 weeb.route("/:id").get(getOne).delete(deleteContent).patch(editOne);
 
 //* [We can use put method and a patch method in order to update our data]
-// todo: [The main difference between out and patch is the put request updated whole data]
+// todo: [The main difference between put and patch is the put request updated whole data]
 // todo: [Whereas the patch method only updates only passed data if data of body is not passed it will not update the body]
